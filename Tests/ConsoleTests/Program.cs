cf8b371eb1fb7ba87114db72abfe677712a6faa6
glebanov <glebanovwebsite@gmail.com>
2020-10-01 15:46:39 +0300

    Добавьте файлы проекта.

---
+++ Tests/ConsoleTests/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text.Json;
+
+namespace ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var to = new MailAddress("glebanovwebsite@gmail.com", "Николай"); //кому отправляем почту
+            var from = new MailAddress("mikolya1212@yandex.ru", "Николай"); //от кого отправляем почту
+
+            var message = new MailMessage(from, to); //создаем почтовое отправление 
+
+            message.Subject = "Заголовок письма от " + DateTime.Now; //Создаем заголовок письма
+            message.Body = "Текст тестового письма +" + DateTime.Now; //Создаем текст письма
+
+            //Создаем клиента smtp сервера
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+
+            //Указываем учетные данные пользователя
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user_name",
+                Password = "PassWord!"
+            };
+
+            client.Send(message);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/WPFTests/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace WPFTests
+{
+    
+    public partial class MainWindow 
+    {
+        public MainWindow()=> InitializeComponent();
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            var to = new MailAddress("glebanovwebsite@gmail.com", "Николай"); //кому отправляем почту
+            var from = new MailAddress("mikolya1212@yandex.ru", "Николай"); //от кого отправляем почту
+
+            var message = new MailMessage(from, to); //создаем почтовое отправление 
+
+            message.Subject = "Заголовок письма от " + DateTime.Now; //Создаем заголовок письма
+            message.Body = "Текст тестового письма +" + DateTime.Now; //Создаем текст письма
+
+            //Создаем клиента smtp сервера
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+            client.EnableSsl = true;
+
+            //Указываем учетные данные пользователя
+            client.Credentials = new NetworkCredential
+            {
+                UserName = LoginEdit.Text,
+                SecurePassword = PasswordEdit.SecurePassword
+            };
+
+            client.Send(message);
+        }
+    }
+}
