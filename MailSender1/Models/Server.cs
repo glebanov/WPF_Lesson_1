6b51d1642fffca572d3ebfe129547879d3bb00e9
glebanov <glebanovwebsite@gmail.com>
2020-10-05 11:13:25 +0300

    Основные модели данных

---
+++ MailSender1/Models/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender1.Models
+{
+    class Server
+    {
+        public string Address { get; set; }
+
+        private int _Port;
+        public int Port
+        {
+            get => _Port;
+            set
+            {
+                if (value < 0 || value >= 65535)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапозоне от 0 до 65534");
+                _Port = value;
+            }
+        }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
