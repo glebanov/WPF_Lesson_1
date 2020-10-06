ebf51184845b50e294fa27ff2d94e039013a3647
glebanov <glebanovwebsite@gmail.com>
2020-10-06 14:31:58 +0300

    Привязка

---
+++ Tests/WPFTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFTests.ViewModels.Base;
+
+namespace WPFTests.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+          
+    }
+}
