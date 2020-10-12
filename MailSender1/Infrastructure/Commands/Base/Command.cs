a7606683ef26fd237676dfad91cc6dd07461c084
glebanov <glebanovwebsite@gmail.com>
2020-10-12 15:16:52 +0300

    Перенос инфраструктуры

---
+++ MailSender1/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MailSender1.Infrastructure.Commands.Base
+{
+    abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+
+        void ICommand.Execute(object parameter) => Execute(parameter);
+
+        protected virtual bool CanExecute(object p) => true;
+
+        protected abstract void Execute(object p);
+    }
+
+}
---
+++ MailSender1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MailSender1.ViewModels.Base;
+
+namespace MailSender1.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
