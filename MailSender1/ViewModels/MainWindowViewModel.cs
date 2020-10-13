5bf6c10a1570b60ce1f707ab4534189a3bb9356e
glebanov <glebanovwebsite@gmail.com>
2020-10-13 10:38:25 +0300

    Команда управления Серверами. Сервис отправки почты.

--- MailSender1/ViewModels/MainWindowViewModel.cs
+++ MailSender1/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using MailSender1.Data;
+using MailSender1.Infrastructure.Commands;
 using MailSender1.Models;
 using MailSender1.ViewModels.Base;
 using System.Collections.ObjectModel;
-
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MailSender1.ViewModels
 {
@@ -72,6 +75,65 @@ namespace MailSender1.ViewModels
             get => _SelectedMessage;
             set => Set(ref _SelectedMessage, value);
         }
+
+        #region Команды
+
+        #region CreateNewServerCommand
+
+        private ICommand _CreateNewServerCommand;
+
+        public ICommand CreateNewServerCommand => _CreateNewServerCommand
+            ??= new LambdaCommand(OnCreateNewServerCommandExecuted, CanCreateNewServerCommandExecuted);
+
+        private bool CanCreateNewServerCommandExecuted(object p) => true;
+
+        private void OnCreateNewServerCommandExecuted(object p)
+        {
+            MessageBox.Show("Создание нового сервера", "Управление серверами");
+        }
+        #endregion
+
+        #region EditServerCommand
+
+        private ICommand _EditServerCommand;
+
+        public ICommand EditServerCommand => _EditServerCommand
+            ??= new LambdaCommand(OnEditServerCommandExecuted, CanEditServerCommandExecuted);
+
+        private bool CanEditServerCommandExecuted(object p) => p is Server || SelectedServer != null;
+
+        private void OnEditServerCommandExecuted(object p)
+        {
+            var server = p as Server ?? SelectedServer;
+            if (server is null) return;
+
+            MessageBox.Show($"Редактирование сервера{server.Address}!", "Управление серверами");
+        }
+        #endregion
+
+        #region DeleteServerCommand
+
+        private ICommand _DeleteServerCommand;
+
+        public ICommand DeleteServerCommand => _DeleteServerCommand
+            ??= new LambdaCommand(OnDeleteServerCommandExecuted, CanDeleteServerCommandExecuted);
+
+        private bool CanDeleteServerCommandExecuted(object p) => p is Server || SelectedServer != null;
+
+        private void OnDeleteServerCommandExecuted(object p)
+        {
+            var server = p as Server ?? SelectedServer;
+            if (server is null) return;
+
+            Servers.Remove(server);
+            SelectedServer = Servers.FirstOrDefault();
+
+            //MessageBox.Show($"Удаление сервера{server.Address}!", "Управление серверами");
+        }
+        #endregion
+
+        #endregion
+
 
         public MainWindowViewModel()
         {
